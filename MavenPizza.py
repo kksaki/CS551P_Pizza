992e72d8710739e70bb253b1250d81763cf49ace
JIAQI SUN <j.sun2.22@abdn.ac.uk>
2023-03-03 00:54:06 +0000

    add some error control

--- MavenPizza.py
+++ MavenPizza.py
@@ -9,9 +9,13 @@ app.config.from_object(__name__)
 db_name = 'Maven_Pizza_Orders.db'
 
 def connect_db():
-    conn = sqlite3.connect(db_name, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
-    conn.row_factory = sqlite3.Row
-    return conn
+    try:
+        conn = sqlite3.connect(db_name, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+        conn.row_factory = sqlite3.Row
+        return conn
+    except sqlite3.Error as e:
+        print(f"Error connecting to database: {e}")
+        return None
 
 def get_db():
     if not hasattr(g, 'sqlite_db'):
@@ -33,33 +37,40 @@ def orders():
     offset = (page - 1) * per_page
 
     db = get_db()
-    cur = db.execute("SELECT COUNT(*) FROM orders")
-    rows = cur.fetchone()[0]
+    try:
+        cur = db.execute("SELECT COUNT(*) FROM orders")
+        rows = cur.fetchone()[0]
 
-    cur = db.execute("SELECT * FROM orders ORDER BY id LIMIT ? OFFSET ?", (per_page, offset))
-    data = cur.fetchall()
+        cur = db.execute("SELECT * FROM orders ORDER BY id LIMIT ? OFFSET ?", (per_page, offset))
+        data = cur.fetchall()
 
-    pagination = Pagination(page=page, per_page=per_page, total=rows, css_framework='bootstrap4')
+        pagination = Pagination(page=page, per_page=per_page, total=rows, css_framework='bootstrap4')
 
-    return render_template('orders.html', rows=data, pagination=pagination)
-
-
-
-
+        return render_template('orders.html', rows=data, pagination=pagination)
+    except sqlite3.Error as e:
+        print(f"Error executing database query: {e}")
+        return render_template('error.html')
 
 @app.route('/order_details/<id>')
 def order_details(id):
     db = get_db()
-    cur = db.execute("select * from orders WHERE id=?", (id,))
-    order = cur.fetchall()
-    cur = db.execute("select * from order_details WHERE OrderID=?", (id,))
-    #逗号代表有多个数据，见老师发的邮件
-    rows = cur.fetchall()
-    return render_template('order_details.html', order=order, rows = rows)
+    try:
+        cur = db.execute("select * from orders WHERE id=?", (id,))
+        order = cur.fetchall()
+        cur = db.execute("select * from order_details WHERE OrderID=?", (id,))
+        rows = cur.fetchall()
+        return render_template('order_details.html', order=order, rows = rows)
+    except sqlite3.Error as e:
+        print(f"Error executing database query: {e}")
+        return render_template('error.html')
 
 @app.route('/pizzas/<id>')
 def pizzas(id):
     db = get_db()
-    cur = db.execute("select * from pizzas WHERE PizzaID=?", (id,))
-    rows = cur.fetchall()
-    return render_template('pizzas.html', rows=rows)
+    try:
+        cur = db.execute("select * from pizzas WHERE PizzaID=?", (id,))
+        rows = cur.fetchall()
+        return render_template('pizzas.html', rows=rows)
+    except sqlite3.Error as e:
+        print(f"Error executing database query: {e}")
+        return render_template('error.html')
